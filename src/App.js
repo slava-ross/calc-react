fffcfca7113adc2705c30e7e048403070d49fe39
Yaroslav Yagodarov <slava-ross@yandex.ru>
2022-08-16 18:05:33 +0400

    second video begin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Total from './components/Total/Total';
+import History from './components/History/History';
+import Operation from './components/Operation/Operation';
 
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <React.Fragment>
+      <header>
+        <h1>Кошелёк</h1>
+        <h2>Калькулятор расходов</h2>
+      </header>
+
+      <main>
+        <div className="container">
+          <Total />
+          <History />
+          <Operation />
+        </div>
+      </main>
+    </React.Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,32 +1,12 @@
 import React from 'react';
-
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
-function Test() {
-  return React.createElement('button', null, 'Privet')
-}
-
-function Test2() {
-  return <button>Privet2</button>;
-}
-
-const FunctionalComponent = (props) => <h1 id="hi">{props.text}</h1>
-const Element = <p>Параграф</p>;
-
-class ClassComponent extends React.Component {
-  render() {
-    return React.createElement('div', {className: 'main'},
-        React.createElement(FunctionalComponent, {text: 'свойство текст'}), Element
-    )
-  }
-}
+import './normalize.css';
+import './style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <ClassComponent />
-    
   </React.StrictMode>
 );
